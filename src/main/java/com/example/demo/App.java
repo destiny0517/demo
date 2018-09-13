408e96ebabe6483e09186bfeec2d1d10d7d82510
destiny <xin.hong0517@gmail.com>
2018-09-13 21:55:09 +0900

    add demo

---
+++ src/main/java/com/example/demo/App.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+	@RequestMapping(value = "nagap", method = RequestMethod.GET)
+	public String getSomething() {
+		return "something";
+	}
+}
